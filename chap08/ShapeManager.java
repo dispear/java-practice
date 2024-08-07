e129e45df01090ca68ec39db8728c09338886ef0
dispear <kkm7hjh@naver.com>
2024-08-07 12:21:53 +0900

    java method practice 0807

--- chap08/ShapeManager.java
+++ chap08/ShapeManager.java
@@ -1,17 +1,31 @@
 package chap08;
 
+import java.util.Arrays;
+
 public class ShapeManager {
     private Shape[] shapes = new Shape[10];
     private int index;
 
     public void addShape(Shape shape) {
         /* 배열에 전달 된 Shape를 추가. 단, 배열의 크기가 부족할 경우 2배로 늘려서 추가. */
+        if(index >= shapes.length){
+            shapes = Arrays.copyOf(shapes, shapes.length * 2);
+        }
         shapes[index++] = shape;
     }
 
     public void removeShape(Shape shape) {
         /* 배열에서 전달 된 Shape를 찾아 제거. 단, 제거 된 인덱스가 비어 있지 않도록 뒤에 있는 객체를 앞으로 당김. */
-        shapes[index--] = null;
+        for (int i = 0; i < index; i++) {
+            if (shapes[i].equals(shape)) {
+                for (int j = i; j < index - 1; j++) {
+                    shapes[j] = shapes[j + 1];
+                }
+                shapes[index - 1] = null;
+                index--;
+                break;
+            }
+        }
     }
 
     public void printAllShapes() {
